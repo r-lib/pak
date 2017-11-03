2f28545300e5bc6cb9464803458e2557e73a2419
Jim Hester <james.f.hester@gmail.com>
2017-11-03 13:52:19 -0400

    Add some library functions

---
+++ R/library.R
@@ -0,0 +1,30 @@
+#' Create a new library
+#'
+#' @param lib The library directory
+#' @export
+lib_create <- function(lib) {
+  #TODO: what do we do if the directory already exists?
+
+  dir.create(lib, recursive = TRUE)
+}
+
+#' Activate or deactivate a given library
+#'
+#' @param action `[character(1)]` Should lib `"replace'`, `"prepend"` or `"append"` the existing library paths.
+#' @inheritParams lib_create
+#' @export
+lib_activate <- function(lib, action = "replace") {
+  lib <- normalizePath(lib, mustWork = TRUE)
+  old <- .libPaths()
+  .libPaths(merge_new(old, lib, action))
+  invisible(old)
+}
+
+#' @rdname lib_activate
+#' @export
+lib_deactivate <- function(lib) {
+  lib <- normalizePath(lib, mustWork = TRUE)
+
+  old <- .libPaths()
+  .libPaths(setdiff(old, lib))
+}
--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,14 @@ is_uri <- function(x) {
   # TODO
   FALSE
 }
+
+# Adapted from withr:::merge_new
+merge_new <- function(old, new, action = match.arg(action, c("replace", "prepend", "append"))) {
+  action <- match.arg(action, c("replace", "prepend", "append"))
+
+  switch(action,
+    prepend = c(new, old),
+    append = c(old, new),
+    replace = new
+  )
+}
