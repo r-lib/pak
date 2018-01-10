2233838d6d810cc1c8a1000347ade7627b34447b
Gábor Csárdi <csardi.gabor@gmail.com>
2018-01-10 11:10:48 +0000

    Fix merge_new

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,7 +2,19 @@
 context("utils")
 
 test_that("merge_new", {
+  expect_identical(merge_new("x", "y"), "y")
+  expect_identical(merge_new("x", "y", "replace"), "y")
+  expect_identical(merge_new("x", "y", "prepend"), c("y", "x"))
+  expect_identical(merge_new("x", "y", "append"), c("x", "y"))
+  expect_error(merge_new("x", "y", "foobar"))
 
+  ## Some special values
+  expect_identical(merge_new("x", NULL), NULL)
+  expect_identical(merge_new(NULL, "x"), "x")
+  expect_identical(merge_new("x", NULL, "append"), "x")
+  expect_identical(merge_new(NULL, "x", "append"), "x")
+  expect_identical(merge_new("x", NULL, "prepend"), "x")
+  expect_identical(merge_new(NULL, "x", "prepend"), "x")
 })
 
 test_that("names2", {
