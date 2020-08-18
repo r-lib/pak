a749c598e7e90a50fadce00e4d27a995b2990b3e
Jim Hester <james.f.hester@gmail.com>
2020-08-18 15:11:26 -0400

    Fix test failures in test-print

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -3,16 +3,16 @@ test_that("print_package_list", {
   withr::local_options(list(rlib_interactive = TRUE, cli.unicode = FALSE))
   expect_message(
     print_package_list(letters[1:5]),
-    "  a, b, c, d and e"
+    "  a, b, c, d, and e"
   )
   expect_message(
     print_package_list(letters[1:5], new_version = 1:5),
-    "  a (1), b (2), c (3), d (4) and e (5)",
+    "  a (1), b (2), c (3), d (4), and e (5)",
     fixed = TRUE
   )
   expect_message(
     print_package_list(letters[1:5], new_version = 1:5, old_version = 0:4),
-    "  a (0 > 1), b (1 > 2), c (2 > 3), d (3 > 4) and e (4 > 5)",
+    "  a (0 > 1), b (1 > 2), c (2 > 3), d (3 > 4), and e (4 > 5)",
     fixed = TRUE
   )
 })
