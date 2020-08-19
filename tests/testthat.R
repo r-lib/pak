13fff99925c417ca7b5dbe2a1356fc8198dce019
Jim Hester <james.f.hester@gmail.com>
2020-08-19 09:42:57 -0400

    Add back the do block in tests/testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,16 +1,16 @@
 library(testthat)
 library(pak)
 
-#do <- function() {
-  #old <- Sys.getenv("R_PKG_CACHE_DIR", NA_character_)
-  #if (is.na(old)) {
-    #on.exit(Sys.unsetenv("R_PKG_CACHE_DIR"))
-  #} else {
-    #on.exit(Sys.setenv("R_PKG_CACHE_DIR" = old))
-  #}
-  #Sys.setenv("R_PKG_CACHE_DIR" = tempfile())
+do <- function() {
+  old <- Sys.getenv("R_PKG_CACHE_DIR", NA_character_)
+  if (is.na(old)) {
+    on.exit(Sys.unsetenv("R_PKG_CACHE_DIR"))
+  } else {
+    on.exit(Sys.setenv("R_PKG_CACHE_DIR" = old))
+  }
+  Sys.setenv("R_PKG_CACHE_DIR" = tempfile())
 
   test_check("pak")
-#}
+}
 
-#do()
+do()
