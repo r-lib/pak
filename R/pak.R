dde75c2b9fd20a444170337d68981915b3595895
Gábor Csárdi <csardi.gabor@gmail.com>
2020-11-09 12:00:25 +0000

    Add pak() function.
    First shot at #176.

---
+++ R/pak.R
@@ -0,0 +1,31 @@
+
+#' Install the required packages
+#'
+#' Install the specified packages, or the ones requiered by
+#' the package or project in the current working directory.
+#'
+#' This is a convenience function:
+#' * If you want to install some packages, it is easier to type
+#'   than [pak::pkg_install()].
+#' * If you want to install all the packages that are needed
+#'   for the development of a package or project, then it is
+#'   easier to type than [pak::local_install_dev_deps()].
+#' * You don't need to remember two functions to install
+#'   packages, just one.
+#'
+#' @param pkg Package names or remote package specifications to install.
+#'   See [pak package sources][pak_package_sources] for details.
+#' @param ... Extra arguments are passed to [pkg_install()] or
+#'   [local_install_dev_deps()].
+#'
+#' @export
+#' @family package functions
+#' @family local package trees
+
+pak <- function(pkg = NULL, ...) {
+  if (is.null(pkg)) {
+    local_install_dev_deps(...)
+  } else {
+    pkg_install(pkg, ...)
+  }
+}
