5a5f8019ffaa2cae07a43677fb8ff663208f194f
Gábor Csárdi <csardi.gabor@gmail.com>
2020-11-22 13:54:54 +0000

    Fix repo name in saved version

--- R/installer.R
+++ R/installer.R
@@ -6,7 +6,7 @@ local({
     path <- file.path(pkgdir, "pak-version.dcf")
     data <- c(
       platform = R.Version()$platform,
-      "github-repository" = Sys.getenv("GITHUB_PORISTORY", "-"),
+      "github-repository" = Sys.getenv("GITHUB_REPOSITORY", "-"),
       "github-sha" = Sys.getenv("GITHUB_SHA", "-"),
       "github-ref" = Sys.getenv("GITHUB_REF", "-")
     )
