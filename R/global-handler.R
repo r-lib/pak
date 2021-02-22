d519b404f7742f622d04694656594b6ca51aae9a
Gábor Csárdi <csardi.gabor@gmail.com>
2021-02-22 12:57:31 +0100

    Remove automatic loading
    And also the setup & remove functions.

--- R/global-handler.R
+++ R/global-handler.R
@@ -1,33 +1,4 @@
 
-setup_global_handler <- function() {
-  if (getRversion() < "4.0") return()
-  if (Sys.getenv("R_PKG_NO_GLOBAL_HANDLER", "") != "") return()
-  packageStartupMessage("Registering pak global handler.")
-  globalCallingHandlers(packageNotFoundError = global_handler)
-}
-
-remove_global_handler <- function(...) {
-  hdrs <- globalCallingHandlers()
-  mine <- vapply(
-    hdrs,
-    function(x) environmentName(environment(x)) == "pak",
-    logical(1)
-  )
-
-  ## Does not seem to work with an empty named list, handle that specially.
-  if (all(mine)) {
-    globalCallingHandlers(NULL)
-  } else if (any(mine)) {
-    globalCallingHandlers(hdrs[!mine])
-  }
-
-  invisible(FALSE)
-}
-
-if (getRversion() < "4.0") {
-  globalVariables("globalCallingHandlers")
-}
-
 global_handler <- function(err) {
   # TODO: is this what we want? Or refine this? E.g. testthat, knitr?
   if (!is_interactive()) return()
