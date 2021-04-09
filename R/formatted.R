7613a67c267e6e59b34e2fba5fd07260a88964e9
Gábor Csárdi <csardi.gabor@gmail.com>
2021-04-09 15:31:07 +0200

    Add pkg_name_check
    Just calls pkgdepends. Also added a way to pre-format objects
    in the subprocess.

---
+++ R/formatted.R
@@ -0,0 +1,18 @@
+
+#' @export
+
+format.pak_preformatted <- function(x, ...) {
+  attr(x, "pak_preformatted")
+}
+
+#' @export
+
+print.pak_preformatted <- function(x, ...) {
+  cat(format(x, ...), sep = "\n")
+}
+
+pak_preformat <- function(x, ...) {
+  attr(x, "pak_preformatted") <- format(x, ...)
+  class(x) <- c("pak_preformatted", class(x))
+  x
+}
