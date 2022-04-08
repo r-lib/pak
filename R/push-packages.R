eeba4a23d3c6c129c21629bdd9b18aeb82da6241
Gábor Csárdi <csardi.gabor@gmail.com>
2022-04-08 11:47:08 +0200

    push_packages typo fix
    [ci skip]

--- R/push-packages.R
+++ R/push-packages.R
@@ -535,7 +535,7 @@ push_packages <- local({
         # rc is also pushed to devel, as devel should be the latest
         p1 <- push_packages(paths, "rc", keep_old, dry_run, cleanup)
         p2 <- push_packages(paths, "devel", keep_old, dry_run, cleanup)
-        return(invisiblr(rbind(p1, p2)))
+        return(invisible(rbind(p1, p2)))
 
       } else if (length(version) >= 4 && version[4] >= 9000) {
         tag <- "devel"
