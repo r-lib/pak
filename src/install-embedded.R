f14ffd049363957d757a7d8dab4941c94e7bfd0c
Gábor Csárdi <csardi.gabor@gmail.com>
2023-11-12 19:18:39 +0100

    Update `Built` platform when cross-compiling

--- src/install-embedded.R
+++ src/install-embedded.R
@@ -23,6 +23,35 @@ get_lib <- function(lib) {
   lib
 }
 
+parse_built <- function(built) {
+  structure(
+    as.list(trimws(strsplit(built, ";", fixed = TRUE)[[1]])),
+    names = c("R", "Platform", "Date", "OStype")
+  )
+}
+
+deparse_built <- function(built) {
+  paste(as.character(unlist(built)), collapse = "; ")
+}
+
+update_description_build <- function(path, platform) {
+  source_path <- file.path(path, "DESCRIPTION")
+  source_desc <- read.dcf(source_path)
+  source_built <- parse_built(source_desc[, "Built"])
+  if (source_built[["Platform"]] != "") {
+    source_built[["Platform"]] <- platform
+    source_desc[, "Built"] <- deparse_built(source_built)
+    write.dcf(source_desc, source_path)
+  }
+
+  binary_path <- file.path(path, "Meta", "package.rds")
+  binary_desc <- readRDS(binary_path)
+  if (binary_desc[["Built"]][["Platform"]] != "") {
+    binary_desc[["Built"]][["Platform"]] <- platform
+    saveRDS(binary_desc, binary_path)
+  }
+}
+
 install_one <- function(pkg, lib = NULL) {
   lib <- get_lib(lib)
 
@@ -72,6 +101,11 @@ install_order <- function() {
 install_all <- function(lib = NULL) {
   pkgs <- install_order()
   if (Sys.getenv("CROSS_COMPILING") == "yes") {
+    # Update 'Built' field in package itself
+    update_description_build(
+      Sys.getenv("R_PACKAGE_DIR"),
+      Sys.getenv("R_TARGET_PLATFORM")
+    )
     lib <- get_lib(lib)
     for (pkg in pkgs) {
       install_one(pkg, lib = paste0(lib, "-", pkg))
@@ -79,6 +113,11 @@ install_all <- function(lib = NULL) {
     for (pkg in pkgs) {
       file.rename(file.path(paste0(lib, "-", pkg), pkg), file.path(lib, pkg))
       unlink(file.path(paste0(lib, "-", pkg)), recursive = TRUE)
+      # Update 'Built' field in dependency
+      update_description_build(
+        file.path(lib, pkg),
+        Sys.getenv("R_TARGET_PLATFORM")
+      )
     }
   } else {
     for (pkg in pkgs) install_one(pkg, lib = lib)
@@ -179,18 +218,17 @@ install_embedded_main <- function() {
     grepl("darwin", pl$target)) {
     # PPM: current is Linux, target is (some) Darwin
     Sys.setenv(CROSS_COMPILING = "yes")
+    Sys.setenv(R_TARGET_PLATFORM = pl$target)
   } else if (grepl("x86_64-apple-darwin", pl$current) &&
     grepl("aarch64-apple-darwin", pl$target)) {
     # Current is Darwin x86_64, target is Darwin aarch64
     Sys.setenv(CROSS_COMPILING = "yes")
-    Sys.setenv(R_MAKEVARS_USER = file.path(
-      getwd(),
-      "Makevars-macos-arm64"
-    ))
+    Sys.setenv(R_TARGET_PLATFORM = pl$target)
   } else if (grepl("x86_64.*linux", pl$current) &&
     grepl("aarch64.*linux, pl$target")) {
     # Current is Linux x86_64, target is Linux aarch64
     Sys.setenv(CROSS_COMPILING = "yes")
+    Sys.setenv(R_TARGET_PLATFORM = pl$target)
   } else {
     # Not cross compiling or cross compiling handled externally
   }
