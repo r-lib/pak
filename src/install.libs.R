cf65206e313f007061d171439ac6426fcd892ea7
Gábor Csárdi <csardi.gabor@gmail.com>
2023-11-17 15:03:59 +0100

    Some internal docs update

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,3 +1,6 @@
+# R CMD INSTALL happily continues even if `make` (via `Makevars`) faile,
+# so we need to throw an error here explicitly.
+
 if (!file.exists("DONE")) {
   stop("Compilation failed.")
 }
