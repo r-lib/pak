b9d4c3f6be787702c21681f42521e6656ddcb107
Gábor Csárdi <csardi.gabor@gmail.com>
2023-11-25 13:53:03 +0100

    pkg_* functions w/o subprocess

--- R/package.R
+++ R/package.R
@@ -1,4 +1,3 @@
-
 #' Install packages
 #'
 #' Install one or more packages and their dependencies into a single
@@ -55,7 +54,7 @@
 #' Upgrade dplyr and all its dependencies:
 #' ```{asciicast pkg-install-upgrade}
 #' pkg_install("dplyr", upgrade = TRUE)
-#' ````
+#' ```
 #'
 #' Install the development version of dplyr:
 #' ```{asciicast pkg-install-gh}
@@ -70,47 +69,33 @@
 
 pkg_install <- function(pkg, lib = .libPaths()[[1L]], upgrade = FALSE,
                         ask = interactive(), dependencies = NA) {
+  load_all_private()
 
   start <- Sys.time()
+  loaded <- loaded_packages(lib)
 
-  status <- remote(
-    function(...) get("pkg_install_make_plan", asNamespace("pak"))(...),
-    list(pkg = pkg, lib = lib, upgrade = upgrade, ask = ask,
-         start = start, dependencies = dependencies,
-         loaded = loaded_packages(lib)))
+  prop <- pkg_data[["ns"]][["pkgdepends"]][["new_pkg_installation_proposal"]](
+    pkg,
+    config = list(library = lib, dependencies = dependencies)
+  )
+  prop$set_solve_policy(if (upgrade) "upgrade" else "lazy")
+  prop$solve()
+  prop$stop_for_solution_error()
 
+  status <- print_install_details(prop, lib, loaded)
   unloaded <- handle_status(status, lib, ask)$unloaded
 
-  inst <- remote(
-    function(...) get("pkg_install_do_plan", asNamespace("pak"))(...),
-    list(proposal = NULL))
+  inst <- pkg_install_do_plan(prop, start)
+
+  # This refers to the desc namespace, and we don't really need it
+  inst$extra <- NULL
 
   if (length(unloaded) > 0) offer_restart(unloaded)
 
   invisible(inst)
 }
 
-pkg_install_make_plan <- function(pkg, lib, upgrade, ask, start,
-                                  dependencies, loaded) {
-  prop <- pkgdepends::new_pkg_installation_proposal(
-    pkg,
-    config = list(library = lib, dependencies = dependencies)
-  )
-
-  ## Solve the dependency graph
-  prop$set_solve_policy(if (upgrade) "upgrade" else "lazy")
-  prop$solve()
-  prop$stop_for_solution_error()
-  pkg_data$tmp <- list(proposal = prop, start = start)
-  print_install_details(prop, lib, loaded)
-}
-
-pkg_install_do_plan <- function(proposal) {
-
-  proposal <- proposal %||% pkg_data$tmp$proposal
-  start  <- pkg_data$tmp$start
-  pkg_data$tmp <- NULL
-
+pkg_install_do_plan <- function(proposal, start) {
   # Actually download packages as needed
   proposal$download()
   proposal$stop_for_download_error()
@@ -149,17 +134,9 @@ pkg_install_do_plan <- function(proposal) {
 #' ```
 
 pkg_status <- function(pkg, lib = .libPaths()) {
-  stopifnot(length(pkg == 1) && is.character(pkg))
-
   load_extra("pillar")
-  remote(
-    function(...) asNamespace("pak")$pkg_status_internal(...),
-    list(pkg = pkg, lib = lib))
-}
-
-pkg_status_internal <- function(pkg, lib = .libPaths()) {
-  st <- lapply(lib, pkgdepends::lib_status, packages = pkg)
-  do.call("rbind_expand", st)
+  load_all_private()
+  pkg_data[["ns"]][["pkgdepends"]][["lib_status"]](lib, pkg)
 }
 
 #' Remove installed packages
@@ -171,17 +148,8 @@ pkg_status_internal <- function(pkg, lib = .libPaths()) {
 #' @family package functions
 
 pkg_remove <- function(pkg, lib = .libPaths()[[1L]]) {
-  remote(
-    function(...) {
-      get("pkg_remove_internal", asNamespace("pak"))(...)
-    },
-    list(pkg = pkg, lib = lib)
-  )
-  invisible()
-}
-
-pkg_remove_internal <- function(pkg, lib) {
-  pr <- pkgdepends::parse_pkg_ref(pkg)
+  load_all_private()
+  pr <- pkg_data[["ns"]][["pkgdepends"]][["parse_pkg_reg"]](pkg)
   suppressMessages(utils::remove.packages(pr$package, lib))
   invisible(pr)
 }
@@ -208,34 +176,33 @@ pkg_remove_internal <- function(pkg, lib) {
 #' ```
 
 pkg_deps <- function(pkg, upgrade = TRUE, dependencies = NA) {
-  stopifnot(length(pkg) == 1 && is.character(pkg))
+  pkg_deps_internal(pkg, upgrade, dependencies)
+}
+
+pkg_deps_internal <- function(pkg, upgrade, dependencies) {
   load_extra("pillar")
-  remote(
-    function(...) {
-      get("pkg_deps_internal", asNamespace("pak"))(...)
-    },
-    list(pkg = pkg, upgrade = upgrade, dependencies = dependencies)
-  )
-}
+  load_all_private()
 
-pkg_deps_internal <- function(pkg, upgrade, dependencies = NA) {
-  deps <- pkg_deps_internal2(pkg, upgrade, dependencies)
-  data <- deps$get_solution()$data
-  # This refers to the 'desc' package namespace, and we don't really need it
-  data$extra <- NULL
-  data
-}
-
-pkg_deps_internal2 <- function(pkg, upgrade, dependencies) {
   dir.create(lib <- tempfile())
   on.exit(rimraf(lib), add = TRUE)
+
   config <- list(library = lib)
   if (!is.null(dependencies)) config$dependencies <- dependencies
-  deps <- pkgdepends::new_pkg_deps(pkg, config = config)
+  deps <- pkg_data[["ns"]][["pkgdepends"]][["new_pkg_deps"]](
+    pkg,
+    config = config
+  )
   if (upgrade) deps$set_solve_policy("upgrade")
   deps$solve()
   deps$stop_for_solution_error()
-  deps
+  data <- deps$get_solution()$data
+  # This refers to the 'desc' package namespace, and we don't need it
+  data$extra <- NULL
+
+  tree <- deps$draw()
+  attr(data, "pkg_deps_tree") <- tree
+
+  data
 }
 
 #' Draw the dependency tree of a package
@@ -257,24 +224,29 @@ pkg_deps_internal2 <- function(pkg, upgrade, dependencies) {
 #' ```
 
 pkg_deps_tree <- function(pkg, upgrade = TRUE, dependencies = NA) {
-  stopifnot(length(pkg == 1) && is.character(pkg))
-  ret <- remote(
-    function(...) {
-      get("pkg_deps_tree_internal", asNamespace("pak"))(...)
-    },
-    list(pkg = pkg, upgrade = upgrade, dependencies = dependencies)
-  )
-  cat(ret$tree, sep = "\n")
-  invisible(ret$data)
+  data <- pkg_deps_internal(pkg, upgrade, dependencies)
+  class(data) <- c("pak_pkg_deps_tree", class(data))
+  data
 }
 
-pkg_deps_tree_internal <- function(pkg, upgrade, dependencies = NA) {
-  deps <- pkg_deps_internal2(pkg, upgrade, dependencies)
-  tree <- deps$draw()
-  data <- deps$get_solution()$data
-  # This refers to the 'desc' package namespace, and we don't really need it
-  data$extra <- NULL
-  list(tree = tree, data = data)
+#' @export
+
+format.pak_pkg_deps_tree <- function(x, ...) {
+  attr(x, "pkg_deps_tree")
+}
+
+#' @export
+
+print.pak_pkg_deps_tree <- function(x, ...) {
+  writeLines(format(x, ...))
+  invisible(x)
+}
+
+#' @export
+
+`[.pak_pkg_deps_tree` <- function(x, i, j, ..., drop = TRUE) {
+  class(x) <- setdiff(class(x), "pak_pkg_deps_tree")
+  NextMethod(x)
 }
 
 #' @rdname lib_status
@@ -323,34 +295,19 @@ pkg_list <- function(lib = .libPaths()[1]) {
 
 pkg_download <- function(pkg, dest_dir = ".", dependencies = FALSE,
                          platforms = NULL, r_versions = NULL) {
-  args <- list(
-    pkg = pkg,
-    dest_dir = dest_dir,
-    dependencies = dependencies,
-    platforms = platforms,
-    r_versions = r_versions
-  )
-
-  dl <- remote(
-    function(...) {
-      get("pkg_download_internal", asNamespace("pak"))(...)
-    },
-    args
-  )
-
   load_extra("pillar")
-  invisible(dl)
-}
+  load_all_private()
 
-pkg_download_internal <- function(pkg, dest_dir = ".", dependencies = FALSE,
-                                  platforms = NULL, r_versions = NULL) {
   mkdirp(dest_dir)
   config <- list(cache_dir = dest_dir, dependencies = dependencies)
   if (!is.null(platforms)) config$platforms <- platforms
   if (!is.null(r_versions)) config$`r-versions` <- r_versions
-  dl <- pkgdepends::new_pkg_download_proposal(pkg, config = config)
+  dl <- pkg_data[["ns"]][["pkgdepends"]][["new_pkg_download_proposal"]](
+    pkg,
+    config = config
+  )
   dl$resolve()
   dl$download()
   dl$stop_for_download_error()
-  dl$get_downloads()
+  invisible(dl$get_downloads())
 }
