74f7cc6a4f272af2cfcef689894f646fe18e0c35
Gábor Csárdi <csardi.gabor@gmail.com>
2025-01-16 19:28:29 +0100

    Fix URLs

--- R/repo.R
+++ R/repo.R
@@ -205,7 +205,7 @@ repo_get_internal <- function(r_version = getRversion(), bioc = TRUE,
 #' * The `RSPM@` prefix is still supported and treated the same way as
 #'   `PPM@`.
 #' * The MRAN service is now retired, see
-#'   <https://techcommunity.microsoft.com/t5/azure-sql-blog/microsoft-r-application-network-retirement/ba-p/3707161>
+#'   `https://techcommunity.microsoft.com/blog/azuresqlblog/microsoft-r-application-network-retirement/3707161`
 #'   for details.
 #' * `MRAN@...` repository specifications now resolve to PPM, but note that
 #'   PPM snapshots are only available from 2017-10-10. See more about this
